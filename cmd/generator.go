cd3a4ff0cae8058397fc8aca243c8275143e2545
Marco Pracucci <marco@pracucci.com>
2023-07-05 14:40:06 +0200

    Improve CLI flag description
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- cmd/generator.go
+++ cmd/generator.go
@@ -29,7 +29,7 @@ var (
 	additionalQueries      = kingpin.Flag("query-additional-queries", "PromQL queries to run in addition to the default.").Strings()
 	tenantsCount           = kingpin.Flag("tenants-count", "Number of tenants to fake.").Default("1").Int()
 	seriesCount            = kingpin.Flag("series-count", "Number of series to generate for each tenant.").Default("1000").Int()
-	seriesChurnPeriod      = kingpin.Flag("series-churn-period", "How frequently the series should churn. 0 to disable churning.").Default("0").Duration()
+	seriesChurnPeriod      = kingpin.Flag("series-churn-period", "How frequently the series should churn. Each series will churn over this duration, and series churning time is spread over the configured period (they will not churn all at the same time). 0 to disable churning.").Default("0").Duration()
 	extraLabelCount        = kingpin.Flag("extra-labels-count", "Number of extra labels to generate for series.").Default("0").Int()
 	serverMetricsPort      = kingpin.Flag("server-metrics-port", "The port where metrics are exposed.").Default("9900").Int()
 )
